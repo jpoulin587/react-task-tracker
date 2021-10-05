70dfdc0d531b95eb7c9104363753541223b2716f
jpoulin587 <jpoulin587a@gmail.com>
2021-10-05 14:00:03 -0500

    add task to console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react"
+import AddTask from "./components/AddTask";
+
 
 function App() {
   const [tasks, setTasks] = useState ([
@@ -23,6 +25,15 @@ function App() {
     }
 ])
 
+//add task
+const addTask = (task) => {
+  console.log(task)
+}
+
+
+
+
+
 // delete task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -37,7 +48,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-    <Header />  
+    <Header /> 
+    <AddTask onAdd={addTask} /> 
     {tasks.length >0 ? (
     <Tasks tasks={tasks} onDelete= {deleteTask}
     onToggle={toggleReminder}/>
