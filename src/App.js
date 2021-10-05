a8ac04f61793c62eac6f91ed3ed8045abf8e62e6
jpoulin587 <jpoulin587a@gmail.com>
2021-10-05 15:07:02 -0500

    show/hide add task form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useState } from "react"
 import AddTask from "./components/AddTask";
 
 
-function App() {
+const App = () => {
+  const [showAddTask, setShowAddTask] = useState (false)
   const [tasks, setTasks] = useState ([
     { 
         id: 1,
@@ -48,7 +49,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
     <Header /> 
-    <AddTask onAdd={addTask} /> 
+    {showAddTask && <AddTask onAdd={addTask} />}
     {tasks.length >0 ? (
     <Tasks tasks={tasks} onDelete= {deleteTask}
     onToggle={toggleReminder}/>
