fbe1ed335ffa3f543625f76c8d8b4cf85dcf481e
jpoulin587 <jpoulin587a@gmail.com>
2021-10-05 14:14:43 -0500

    add task to list

--- src/App.js
+++ src/App.js
@@ -27,12 +27,11 @@ function App() {
 
 //add task
 const addTask = (task) => {
-  console.log(task)
+  const id= Math.floor(Math.random() * 10000) +1
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask])
+  
 }
-
-
-
-
 
 // delete task
 const deleteTask = (id) => {
