db8ab631dbc84f42816e261d49a1581d0c9730a3
jpoulin587 <jpoulin587a@gmail.com>
2021-10-06 02:00:26 -0500

    add toggle reminder flag

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ const fetchTasks = async () => {
   return(data)
 }
 
+//fetch one task
+const fetchTask = async (id) => {
+  const res = await fetch(`http://localhost:5000/tasks/${id}`)
+  const data = await res.json()
+
+  return(data)
+}
 
 //add task
 const addTask = async (task) => {
@@ -57,8 +64,22 @@ const deleteTask = async (id) => {
 }
 
 //toggle reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task))
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updatedTask = {...taskToToggle, 
+    reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(updatedTask)
+    })
+    const data = await res.json()
+
+  setTasks(tasks.map((task) => 
+  task.id === id ? {...task, reminder: data.reminder } : task))
 }
 
 
